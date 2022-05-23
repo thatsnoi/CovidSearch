a851a772a34bff175cb52b4aaa8b2584ec8d8d4f
Noah Jadallah <noah.jadallah@gmx.net>
2022-05-23 21:02:26 +0200

    add logging

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import argparse
 from utils import download_dataset
 from query_gen_and_train import gen_queries, train_bi_encoder
 from test import test
+import neptune.new as neptune
 
 parser = argparse.ArgumentParser(description='Parameters for TREC-Covid IR.')
 parser.add_argument('--sample_size',        type=int, default=None, help='Corpus sample size')
 parser.add_argument('--gen',                type=bool, default=True, help='Generate queries')
-parser.add_argument('--pretrained_model',   type=str, default=True,
+parser.add_argument('--pretrained_model',   type=str, default='dmis-lab/biobert-v1.1',
                     help='Pretrained huggingface model for bi-encoder training.')
 parser.add_argument('--num_epochs',             type=int, default=10, help='Epochs for bi-encoder training.')
 
@@ -15,7 +16,36 @@ args = parser.parse_args()
 
 dataloader, data_path = download_dataset()
 
-gen_queries(dataloader, data_path, sample_size=args.sample_size)
-model_save_path = train_bi_encoder(data_path, num_epochs=args.num_epochs)
+run = neptune.init(
+    project="noahjadallah/TREC-Covid",
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlNzgwNzhlNy1iMDVlLTQwNWUtYWJlYS04NWMxNjA0YmQ3ODAifQ==",
+)
 
-test(dataloader, model_save_path, args.sample_size)
+params = {
+    "pretrained_model": args.pretrained_model,
+    "sample_size": args.sample_size,
+    "num_epochs": args.num_epochs
+    }
+run["parameters"] = params
+
+gen_queries(dataloader, data_path, sample_size=args.sample_size)
+model_save_path = train_bi_encoder(data_path, num_epochs=args.num_epochs, model_name=args.pretrained_model)
+
+ndcg, _map, _map, precision = test(dataloader, model_save_path, args.sample_size)
+
+run["eval"] = {
+    "ndcg@1": ndcg[0],
+    "ndcg@5": ndcg[1],
+    "ndcg@10": ndcg[2],
+    "map@1": _map[0],
+    "map@5": _map[1],
+    "map@10": _map[2],
+    "recall@1": _map[0],
+    "recall@5": _map[1],
+    "recall@10": _map[2],
+    "precision@1": precision[0],
+    "precision@5": precision[1],
+    "precision@10": precision[2],
+}
+
+run.stop()
