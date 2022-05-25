f3400e23a64d299b59c5fbaffeac70fafc34f333
Noah Jadallah <noah.jadallah@gmx.net>
2022-05-25 11:31:05 +0200

    add bi-encoder from neptune test

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ parser.add_argument('--pretrained_model',   type=str, default='dmis-lab/biobert-
 parser.add_argument('--num_epochs',         type=int, default=10, help='Epochs for bi-encoder training.')
 parser.add_argument('--gen', action='store_true')
 parser.add_argument('--no-gen', dest='gen', action='store_false')
+parser.add_argument('--bi_encoder', type=str, default=None, help='Bi-Encoder from neptune')
 parser.set_defaults(gen=True)
 
 args = parser.parse_args()
@@ -40,15 +41,24 @@ else:
 run["dataset/gen/gen_queries.jsonl"].upload(path.join(data_path, "gen-queries.jsonl"))
 run["dataset/gen/train.tsv"].upload(path.join(data_path, "gen-qrels/train.tsv"))
 
-model_save_path = train_bi_encoder(data_path, num_epochs=args.num_epochs, model_name=args.pretrained_model)
+if args.bi_encoder:
+    model_save_path = train_bi_encoder(data_path, num_epochs=args.num_epochs, model_name=args.pretrained_model)
+else:
+    project = neptune.init(
+        project="noahjadallah/TREC-Covid",
+        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlNzgwNzhlNy1iMDVlLTQwNWUtYWJlYS04NWMxNjA0YmQ3ODAifQ==",
+        run=args.bi_encoder
+    )
+    project["model/bi-encoder"].download()
 
-run["model/bi-encoder"].upload_files(model_save_path)
 
-ndcg, _map, recall, precision = test(dataloader, model_save_path, args.sample_size)
-
-run["eval/ndcg"] = ndcg
-run["eval/map"] = _map
-run["eval/recall"] = recall
-run["eval/precision"] = precision
+# run["model/bi-encoder"].upload_files(model_save_path)
+#
+# ndcg, _map, recall, precision = test(dataloader, model_save_path, args.sample_size)
+#
+# run["eval/ndcg"] = ndcg
+# run["eval/map"] = _map
+# run["eval/recall"] = recall
+# run["eval/precision"] = precision
 
 run.stop()
