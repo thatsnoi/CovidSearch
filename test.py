3f6f94efd67567e30a85fc9df1c5a92a9cdc90b1
Noah Jadallah <noah.jadallah@gmx.net>
2022-05-26 14:41:27 +0200

    add bm25

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from beir.reranking.models import CrossEncoder
 from beir.reranking import Rerank
 import random
 import logging
+from bm25 import bm25
 from utils import load_queries_query, load_queries_narrative
 import types
 
@@ -17,15 +18,15 @@ def test(dataloader, model_path, sample_size, score_function="dot"):
     # Using "text" query
     corpus, queries_text, qrels = dataloader.load(split="test")
 
-    # Using "query" query
-    dataloader.queries = {}
-    dataloader._load_queries = types.MethodType(load_queries_query, dataloader)
-    _, queries_query, _ = dataloader.load(split="test")
-
-    # Using "narrative" query
-    dataloader.queries = {}
-    dataloader._load_queries = types.MethodType(load_queries_narrative, dataloader)
-    _, queries_narrative, _ = dataloader.load(split="test")
+    # # Using "query" query
+    # dataloader.queries = {}
+    # dataloader._load_queries = types.MethodType(load_queries_query, dataloader)
+    # _, queries_query, _ = dataloader.load(split="test")
+    #
+    # # Using "narrative" query
+    # dataloader.queries = {}
+    # dataloader._load_queries = types.MethodType(load_queries_narrative, dataloader)
+    # _, queries_narrative, _ = dataloader.load(split="test")
 
     if sample_size is not None:
         random.seed(55)
@@ -38,22 +39,25 @@ def test(dataloader, model_path, sample_size, score_function="dot"):
     print('Results for "text" query')
     results_text = retriever.retrieve(corpus, queries_text)
     retriever.evaluate(qrels, results_text, [1, 5, 10, 20])
-    print(results_text)
 
-    print('Results for "query" query')
-    results_query = retriever.retrieve(corpus, queries_query)
-    retriever.evaluate(qrels, results_query, [1, 5, 10, 20])
+    print('Results with BM25')
+    results_bm25 = bm25(dataloader, sample_size=sample_size)
+    retriever.evaluate(qrels, results_bm25, [1, 5, 10, 20])
 
-    print('Results for "narrative" query')
-    results_narrative = retriever.retrieve(corpus, queries_narrative)
-    retriever.evaluate(qrels, results_narrative, [1, 5, 10, 20])
+    # print('Results for "query" query')
+    # results_query = retriever.retrieve(corpus, queries_query)
+    # retriever.evaluate(qrels, results_query, [1, 5, 10, 20])
+    #
+    # print('Results for "narrative" query')
+    # results_narrative = retriever.retrieve(corpus, queries_narrative)
+    # retriever.evaluate(qrels, results_narrative, [1, 5, 10, 20])
 
     cross_encoder_model = CrossEncoder('cross-encoder/ms-marco-electra-base')
     reranker = Rerank(cross_encoder_model, batch_size=128)
 
     # Fuse rankings
     print("Fusing rankings...")
-    fused_rankings = rrf([results_text, results_query, results_narrative])
+    fused_rankings = rrf([results_text, results_bm25])
     retriever.evaluate(qrels, fused_rankings, [1, 5, 10, 20])
 
     # Rerank top-100 results retrieved by bi encoder model
